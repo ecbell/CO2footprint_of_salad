6ffdc5cb85160fa5ad29acb90bc0df781cc6c144
ecbell <ebell691@gmail.com>
2021-10-29 13:21:30 -0500

    Addded to package.json

--- dist/main.js
+++ dist/main.js
@@ -6,14 +6,14 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (() => { // webpackBootstrap
+/******/ (function() { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ (function() {
 
 eval("// Your entry file is src / index.js.I'd recommend adding \n// a DOMContentLoaded event listener, and in that callback doing \n// some basic DOM manipulation. \n// Test that it works in the browser; \n// this way, you know your webpack is working and everything is hooked up correctly.//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9DTzJmb290cHJpbnRfb2Zfc2FsYWQvLi9zcmMvaW5kZXguanM/YjYzNSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiLy8gWW91ciBlbnRyeSBmaWxlIGlzIHNyYyAvIGluZGV4LmpzLkknZCByZWNvbW1lbmQgYWRkaW5nIFxuLy8gYSBET01Db250ZW50TG9hZGVkIGV2ZW50IGxpc3RlbmVyLCBhbmQgaW4gdGhhdCBjYWxsYmFjayBkb2luZyBcbi8vIHNvbWUgYmFzaWMgRE9NIG1hbmlwdWxhdGlvbi4gXG4vLyBUZXN0IHRoYXQgaXQgd29ya3MgaW4gdGhlIGJyb3dzZXI7IFxuLy8gdGhpcyB3YXksIHlvdSBrbm93IHlvdXIgd2VicGFjayBpcyB3b3JraW5nIGFuZCBldmVyeXRoaW5nIGlzIGhvb2tlZCB1cCBjb3JyZWN0bHkuIl0sImZpbGUiOiIuL3NyYy9pbmRleC5qcy5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/index.js\n");
 
@@ -23,7 +23,7 @@ eval("// Your entry file is src / index.js.I'd recommend adding \n// a DOMConten
 /*!************************!*\
   !*** ./src/index.scss ***!
   \************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguc2Nzcy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9DTzJmb290cHJpbnRfb2Zfc2FsYWQvLi9zcmMvaW5kZXguc2Nzcz85NzQ1Il0sInNvdXJjZXNDb250ZW50IjpbIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpblxuZXhwb3J0IHt9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/index.scss\n");
@@ -37,15 +37,15 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
+/******/ 	!function() {
 /******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
+/******/ 		__webpack_require__.r = function(exports) {
 /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	})();
+/******/ 	}();
 /******/ 	
 /************************************************************************/
 /******/ 	
