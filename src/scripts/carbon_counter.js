412a838de7e23fa5a57b2922b195a54ddbd32bc7
ecbell <ebell691@gmail.com>
2021-10-31 12:15:41 -0500

    complete skeleton functionality

--- src/scripts/carbon_counter.js
+++ src/scripts/carbon_counter.js
@@ -1,22 +1,18 @@
 class carbonCounter {
+
   constructor(parent, ingredients) {
     this.ingredients = ingredients;
     this.parent = parent;
     this.counter = 0
 
+    document.querySelector("#carbon-counter").innerHTML = this.counter
     document.getElementById("dropdown").addEventListener('click', this.handleclick.bind(this));
   }
 
   handleclick(e) {
     if (e.currentTarget !== e.target) {
-      // this.ingredients[e.target.innerText].averageCO2;
-      // let clickImage = document.querySelector(".image")
-      // console.log(this.counter)
-      // console.log(this.ingredients[e.target.innerText].averageCO2)
-      document.querySelector("#carbon-counter").innerHTML = this.counter
-      this.counter += this.ingredients[e.target.innerText].averageCO2
-      // document.querySelector(".carbon-counter").innerHTML = counter
-      console.log(this.counter)
+      let count = this.counter += this.ingredients[e.target.innerText].averageCO2
+      document.querySelector("#carbon-counter").innerHTML = count
     }
   }
 
